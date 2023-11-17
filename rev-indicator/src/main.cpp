b775921709b4f9ebc3d788aeeae9a894268b7cc9
Tim Fleskes <t.fleskes@gemini-et.nl>
2023-11-17 16:58:43 +0100

    Working for LED Strip containing 25 leds, last 5 leds are blue shift_leds

--- rev-indicator/src/main.cpp
+++ rev-indicator/src/main.cpp
@@ -7,41 +7,21 @@
 enum leds_e : uint8_t
 {
   GREEN = 0,
-  YELLOW = 4,
-  RED = 8
+  YELLOW = 9,
+  RED = 16
 };
 
-const uint8_t REV_LED_COUNT = 10;
-const uint8_t GREEN_LED_COUNT = 4;
-const uint8_t YELLOW_LED_COUNT = 3;
-const uint8_t RED_LED_COUNT = 3;
-
-led rev_led[] = {
-  led(LED_GREEN1),
-  led(LED_GREEN2),
-  led(LED_GREEN3),
-  led(LED_GREEN4),
-  led(LED_YELLOW1),
-  led(LED_YELLOW2),
-  led(LED_YELLOW3),
-  led(LED_RED1),
-  led(LED_RED2),
-  led(LED_RED3),
-};
-
-const uint8_t SHIFT_LED_COUNT = 2;
-
-led shift_leds[] = {
-  led(LED_BLUE1),
-  led(LED_BLUE2),
-};
+const uint8_t GREEN_LED_COUNT = 9;
+const uint8_t YELLOW_LED_COUNT = 7;
+const uint8_t RED_LED_COUNT = 6;
+const uint8_t SHIFT_LED_COUNT = 3;
 
 // Timer durations
 utils::timer rev_timer(50);
 utils::timer shift_timer(100);
 
-void toggle_leds(led *leds, uint8_t count);
-void set_leds(led *leds, uint8_t count, bool on);
+void toggle_leds(CRGB *leds, uint8_t count, CRGB::HTMLColorCode color);
+void set_leds(CRGB *leds, uint8_t count, CRGB::HTMLColorCode color);
 
 CRGB leds[NUM_LEDS];
 
@@ -53,8 +33,8 @@ void setup()
   Serial.println("Program Start");
   rev_timer.start();
 
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(30);
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(100);
 }
 
 void loop()
@@ -64,48 +44,46 @@ void loop()
 
   if (rev_timer.check())
   {
-    set_leds(rev_led, REV_LED_COUNT, false);
-    set_leds(rev_led, mapped, true);
-    // fill_solid(leds, mapped, CRGB::Green);
-    FastLED.show();
+    set_leds(leds, REV_LEDS, CRGB::Black);
+
+    for (int i = 0; i < min(mapped, REV_LEDS); i++)
+    {
+      uint8_t green = map(i, 0, REV_LEDS - 1, 255, 0);
+      uint8_t red = map(i, 0, REV_LEDS - 1, 0, 255);
+
+      leds[i] = CRGB(red, green, 0);
+    }
+
+    if (mapped > REV_LEDS)
+    {
+      if (millis() % 200 < 100)
+      {
+        set_leds(leds + REV_LEDS, SHIFT_LEDS, CRGB::Blue);
+      }
+      else
+      {
+        set_leds(leds + REV_LEDS, SHIFT_LEDS, CRGB::Black);
+      }
+    }
+    else if (reading <= 1000)
+    {
+      set_leds(leds + REV_LEDS, SHIFT_LEDS, CRGB::Black);
+    }
   }
 
-  if (shift_timer.check())
-  {
-    if (reading > 1000)
-    {
-      toggle_leds(shift_leds, SHIFT_LED_COUNT);
-      FastLED.show();
-    }
-    else
-    {
-      set_leds(shift_leds, SHIFT_LED_COUNT, false);
-      FastLED.show();
-    }
-  }
+  FastLED.show();
 }
 
 // Enables blinking blue shift led
-void toggle_leds(led *leds, uint8_t count)
+void toggle_leds(CRGB *leds, uint8_t count, CRGB::HTMLColorCode color)
 {
   for (uint8_t i = 0; i < count; ++i)
-  {
-    leds[i].toggle();
-  }
+    leds[i] = color;
 }
 
 // Enables rev leds
-void set_leds(led *leds, uint8_t count, bool on)
+void set_leds(CRGB *leds, uint8_t count, CRGB::HTMLColorCode color)
 {
   for (uint8_t i = 0; i < count; ++i)
-  {
-    if (on)
-    {
-      leds[i].on();
-    }
-    else
-    {
-      leds[i].off();
-    }
-  }
+    leds[i] = color;
 }
